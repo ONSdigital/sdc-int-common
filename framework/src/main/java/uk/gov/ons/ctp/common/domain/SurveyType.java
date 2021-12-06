a38e3c84590ee19a67d60a330fdc75fed9240ec1
Rob Catling <github@rpc-soft.com>
2021-12-06 08:18:25 +0000

    SOCINT-242 add basename to SurveyType

--- framework/src/main/java/uk/gov/ons/ctp/common/domain/SurveyType.java
+++ framework/src/main/java/uk/gov/ons/ctp/common/domain/SurveyType.java
@@ -4,10 +4,16 @@ public enum SurveyType {
   SOCIAL("social"),
   SIS("sis");
 
+  private String basename;
   private String suffix;
 
-  private SurveyType(String pattern) {
-    this.suffix = pattern + ".json";
+  private SurveyType(String basename) {
+    this.basename = basename;
+    this.suffix = basename + ".json";
+  }
+
+  public String getBasename() {
+    return basename;
   }
 
   public static SurveyType fromSampleDefinitionUrl(String url) {
