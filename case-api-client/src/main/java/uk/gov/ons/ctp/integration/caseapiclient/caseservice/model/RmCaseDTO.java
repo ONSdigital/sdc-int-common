6c65c0cc446bd09028db987a1dfa1ffe0440c610
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2021-12-17 11:01:56 +0000

    SOCINT-332 changed to LocalDateTime

--- case-api-client/src/main/java/uk/gov/ons/ctp/integration/caseapiclient/caseservice/model/RmCaseDTO.java
+++ case-api-client/src/main/java/uk/gov/ons/ctp/integration/caseapiclient/caseservice/model/RmCaseDTO.java
@@ -1,10 +1,16 @@
 package uk.gov.ons.ctp.integration.caseapiclient.caseservice.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.Data;
 
 @Data
@@ -13,9 +19,11 @@ public class RmCaseDTO {
 
   private String caseRef;
 
-  private Date createdDateTime;
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+  private LocalDateTime createdDateTime;
 
-  private Date lastUpdated;
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+  private LocalDateTime lastUpdated;
 
   private List<EventDTO> caseEvents;
 
