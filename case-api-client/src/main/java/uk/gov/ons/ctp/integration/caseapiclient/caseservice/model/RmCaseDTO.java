46a492e1284d58ef5b82060fac7c2b4d9ac0f9db
Phil Whiles <phil.whiles@gmail.com>
2022-01-10 12:19:07 +0000

    SOCINT-333 make compatible with actual RM Case API datetime

--- case-api-client/src/main/java/uk/gov/ons/ctp/integration/caseapiclient/caseservice/model/RmCaseDTO.java
+++ case-api-client/src/main/java/uk/gov/ons/ctp/integration/caseapiclient/caseservice/model/RmCaseDTO.java
@@ -1,12 +1,11 @@
 package uk.gov.ons.ctp.integration.caseapiclient.caseservice.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 
@@ -16,10 +15,8 @@ public class RmCaseDTO {
 
   private String caseRef;
 
-  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
   private LocalDateTime createdDateTime;
 
-  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
   private LocalDateTime lastUpdated;
 
   private List<EventDTO> caseEvents;
