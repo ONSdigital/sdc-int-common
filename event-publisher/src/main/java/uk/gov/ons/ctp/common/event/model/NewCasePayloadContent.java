a53e362a765954e805e323297d70ba2d246072a9
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2022-02-02 15:22:00 +0000

    SOCINT-395 Added attribute names to support RH new case creation

--- event-publisher/src/main/java/uk/gov/ons/ctp/common/event/model/NewCasePayloadContent.java
+++ event-publisher/src/main/java/uk/gov/ons/ctp/common/event/model/NewCasePayloadContent.java
@@ -15,7 +15,22 @@ import lombok.NoArgsConstructor;
 public class NewCasePayloadContent implements EventPayload {
 
   //TODO : FLEXIBLE CASE
-  
+  // Sample attributes
+  public static final String ATTRIBUTE_SCHOOL_ID = "schoolId";
+  public static final String ATTRIBUTE_SCHOOL_NAME = "schoolName";
+  public static final String ATTRIBUTE_CONSENT_GIVEN_TEST = "consentGivenTest";
+  public static final String ATTRIBUTE_CONSENT_GIVEN_SURVEY = "consentGivenSurvey";
+
+  // Sample sensitive attributes
+  public static final String ATTRIBUTE_FIRST_NAME = "firstName";
+  public static final String ATTRIBUTE_LAST_NAME = "lastName";
+  public static final String ATTRIBUTE_CHILD_FIRST_NAME = "childFirstName";
+  public static final String ATTRIBUTE_CHILD_MIDDLE_NAMES = "childMiddleNames";
+  public static final String ATTRIBUTE_CHILD_LAST_NAME = "childLastName";
+  public static final String ATTRIBUTE_CHILD_DOB = "childDob";
+  public static final String ATTRIBUTE_MOBILE_NUMBER = "mobileNumber";
+  public static final String ATTRIBUTE_EMAIL_ADDRESS = "emailAddress";
+
   private UUID caseId;
   private UUID collectionExerciseId;
 
